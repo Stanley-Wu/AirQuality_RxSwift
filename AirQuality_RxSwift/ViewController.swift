2b6c365348544ce3b4d2b3ace4113c9f6bc4a59d
StanleyWu <stanley.wu@hq.dlink.com>
2020-08-20 16:23:07 +0800

    Initial Commit

---
+++ AirQuality_RxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirQuality_RxSwift
+//
+//  Created by Dlink on 2020/8/20.
+//  Copyright © 2020 Dlink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
